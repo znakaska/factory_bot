f04978a53ce51420f9701ad9dab402bc9f2199a8
Joshua Clayton <jclayton@thoughtbot.com>
2013-09-20 10:12:12 -0400

    Extract attribute/override assignment to modules

---
+++ lib/factory_girl/attributes_module_generator.rb
@@ -0,0 +1,25 @@
+module FactoryGirl
+  # @api private
+  class AttributesModuleGenerator
+    def initialize(attributes)
+      @attributes = attributes
+    end
+
+    def to_module
+      mod = Module.new
+
+      @attributes.each do |attribute|
+        mod.send :define_method, attribute.name do
+          if @cached_attributes.key?(attribute.name)
+            @cached_attributes[attribute.name]
+          else
+            @cached_attributes[attribute.name] = instance_exec &attribute.to_proc
+          end
+        end
+      end
+
+      mod
+    end
+  end
+end
+
